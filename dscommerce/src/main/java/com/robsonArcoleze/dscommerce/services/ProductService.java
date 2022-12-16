777b34a726ba65a0c26d21206a6c88b6b22b0c4e
RobsonDev <robson.arcoleze29@gmail.com>
2022-12-16 16:00:24 -0300

    Created Product service

---
+++ dscommerce/src/main/java/com/robsonArcoleze/dscommerce/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.robsonArcoleze.dscommerce.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.robsonArcoleze.dscommerce.DTO.ProductDTO;
+import com.robsonArcoleze.dscommerce.entities.Product;
+import com.robsonArcoleze.dscommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Product product = repository.findById(id).get();
+		return new ProductDTO(product);
+		
+	}
+}
