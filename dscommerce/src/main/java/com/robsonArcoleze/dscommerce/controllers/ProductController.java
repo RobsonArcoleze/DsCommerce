50fa7aeb23d2ef1c84f45e1a2f21e5dd42e2b33c
RobsonDev <robson.arcoleze29@gmail.com>
2022-12-16 15:56:35 -0300

    Corrigido injeção de depencias, e adicionado end-point de busca pelo Id

--- dscommerce/src/main/java/com/robsonArcoleze/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/robsonArcoleze/dscommerce/controllers/ProductController.java
@@ -1,26 +1,23 @@
 package com.robsonArcoleze.dscommerce.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.robsonArcoleze.dscommerce.entities.Product;
-import com.robsonArcoleze.dscommerce.repositories.ProductRepository;
+import com.robsonArcoleze.dscommerce.DTO.ProductDTO;
+import com.robsonArcoleze.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
 	@Autowired
-	private ProductRepository repository;
+	private ProductService service;
 	
-	@GetMapping
-	public String teste() {
-		Optional<Product> result = repository.findById(1L);
-		Product product = result.get();
-		return product.getName();
+	@GetMapping(value = "/{id}")
+	public ProductDTO findById(@PathVariable Long id) {
+		return service.findById(id);
 	}
 }
