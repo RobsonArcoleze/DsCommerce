08510ae33538e6ef811ef024f97c24f8661e9450
RobsonDev <robson.arcoleze29@gmail.com>
2022-12-23 17:23:21 -0300

    Bean Validation

---
+++ backend/src/main/java/com/robsonArcoleze/dscommerce/DTO/FildMessage.java
@@ -0,0 +1,20 @@
+package com.robsonArcoleze.dscommerce.DTO;
+
+public class FildMessage {
+
+	private String fildName;
+	private String message;
+	
+	public FildMessage(String fildName, String message) {
+		this.fildName = fildName;
+		this.message = message;
+	}
+	public String getFildName() {
+		return fildName;
+	}
+	public String getMessage() {
+		return message;
+	}
+	
+	
+}
---
+++ backend/src/main/java/com/robsonArcoleze/dscommerce/DTO/ValidationError.java
@@ -0,0 +1,25 @@
+package com.robsonArcoleze.dscommerce.DTO;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError  extends CustomError{
+
+	private List<FildMessage> errors = new ArrayList<>();
+	
+	
+	public ValidationError(Instant timestamp, Integer status, String error, String path) {
+		super(timestamp, status, error, path);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public List<FildMessage> getErrors() {
+		return errors;
+	}
+	
+	public void addError(String fildName, String message) {
+		errors.add(new FildMessage(fildName, message));
+	}
+}
