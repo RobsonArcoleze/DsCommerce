cf8317c58bbb2f049262176900478686928f7e00
RobsonArcoleze <robson.arcoleze29@gmail.com>
2023-02-27 20:49:49 -0300

    feat: Implementado doc nos endpoints
    Documentado API com swagger na camada de controllers

--- backend/src/main/java/com/robsonArcoleze/dscommerce/controllers/ProductController.java
+++ backend/src/main/java/com/robsonArcoleze/dscommerce/controllers/ProductController.java
@@ -19,28 +19,63 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.robsonArcoleze.dscommerce.DTO.ProductDTO;
 import com.robsonArcoleze.dscommerce.services.ProductService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/products")
+@Tag(name = "Products", description = "Endpoint for Managing Products")
 public class ProductController {
 
 	@Autowired
 	private ProductService service;
 	
+
 	@GetMapping(value = "/{id}")
+	@Operation(summary = "Finds a product", description = "Finds a product", tags = {"Products"}, 
+	responses = {
+			@ApiResponse(description = "Success", responseCode = "200", 
+					content = @Content(schema = @Schema(implementation = ProductDTO.class))	),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+	})
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		ProductDTO dto = service.findById(id);
 		return ResponseEntity.ok(dto);
 	}
 	
 	@GetMapping
+	@Operation(summary = "Finds all products", description = "Finds all products", tags = {"Products"}, 
+	responses = {
+			@ApiResponse(description = "Success", responseCode = "200", 
+					content = @Content(schema = @Schema(implementation = ProductDTO.class))	),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+	})
 	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
 		Page<ProductDTO> dto = service.findAll(pageable);
 		return ResponseEntity.ok(dto);
 	}
 	
 	@PostMapping
+	@Operation(summary = "Add a new product", description = "Add a new product", tags = {"Products"}, 
+	responses = {
+			@ApiResponse(description = "Success", responseCode = "200", 
+					content = @Content(schema = @Schema(implementation = ProductDTO.class))	),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+	})
 	public  ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
@@ -48,12 +83,27 @@ public class ProductController {
 	}
 	
 	@PutMapping(value = "/{id}")
+	@Operation(summary = "Update a product", description = "Update a product", tags = {"Products"}, 
+	responses = {
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+	})
 	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);
 	}
 
 	@DeleteMapping(value = "/{id}")
+	@Operation(summary = "Delete a product", description = "Delete a product", tags = {"Products"}, 
+	responses = {
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+	})
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
